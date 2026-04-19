1786af5d1dd3633f58f1e969339c67496525fc05
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-04-19 23:23:58 +0600

    fixing home and connecting it to login page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,24 @@
-console.log("login functionality done")
+document.getElementById("login-btn").addEventListener("click", function(){
+// get the mobile number
+const numberInput = document.getElementById("input-num");
+const contactNumber = numberInput.value;
+console.log(contactNumber)
+// get the pin
+const pinInput = document.getElementById("input-pin");
+const pin = pinInput.value;
+console.log(pin)
+// match pin and number
+if(contactNumber == "01234567890" && pin=="1234"){
+    alert("login done");
+    // window.location.replace("./home.html")
+    window.location.assign("./home.html")
+}else{
+alert("login Failed");
+return;
+
+}
+// true--- >home page 
+// false ---> error
+
+})
+
